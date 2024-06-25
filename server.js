d5929933e22bf3551bf0f4ad598792fe1404dbf0
Fadel <mfiva213@gmail.com>
2024-06-25 07:58:20 +0700

    Bismillah bisa deploy

---
+++ server.js
@@ -0,0 +1,16 @@
+// app.js
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define the port
+const PORT = process.env.PORT || 8080;
+
+// Start the server
+app.listen(PORT, () => {
+    console.log('Server is running on http://localhost:${PORT}');
+});
